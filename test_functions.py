2d561861f4baa439113d65b508927349f7a482a7
UmbraDracona <shadowfire0117@gmail.com>
2020-03-04 22:52:52 -0600

    reformat of gb scraper

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,6 @@
 from scraper_lc import LivrariaCultura
 from scraper_sd import Scribd
+from scraper_gb import GoogleBooks
 
 from objects import SiteBookData
 
@@ -8,16 +9,20 @@ def get_book_site(slug):
         return LivrariaCultura()
     elif slug == 'sd':
         return Scribd()
+    elif slug == 'gb':
+        return GoogleBooks()
 
 #sample book_id from each site:
 lc = 'bible-commentary-the-gospel-of-john-2012895340/p'
 sd = 'book/295105408/The-MacArthur-Study-Bible'
+gb = "books?id=LhTuQAAACAAJ"
 
 
-slug = 'lc'
+slug = 'gb'
 site = get_book_site(slug)
-url = site.convert_book_id_to_url(lc)
+url = site.convert_book_id_to_url(gb)
 sbd = site.get_book_data_from_site(url)
-print(site.find_book_matches_at_site(sbd))
+#print(site.find_book_matches_at_site(sbd))
 
 #open("lcsample2.txt", "wb").write(site.find_book_matches_at_site(sbd))
+sbd.pr()
